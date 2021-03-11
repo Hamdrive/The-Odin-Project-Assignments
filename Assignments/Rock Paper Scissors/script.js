c52ce8fe2d9e9c5ee33648a079ac9d8af2a16973
Hamza <hamza.husein017@gmail.com>
2021-03-11 18:34:30 +0530

    loop for multiple plays

--- Assignments/Rock Paper Scissors/script.js
+++ Assignments/Rock Paper Scissors/script.js
@@ -13,17 +13,23 @@ let letsPlay = () => {
     let userMoveIndex = playableMoves.indexOf(userPlay);
     let computerMoveIndex = playableMoves.indexOf(computer)
     if(computerMoveIndex === userMoveIndex){
+        console.log(computerMoveIndex, userMoveIndex)
         alert("IT'S A TIE")
     }
     else if (computerMoveIndex > userMoveIndex){
+        console.log(computerMoveIndex, userMoveIndex);
         alert("COMPUTER WINS");
     }
-    else{
+    else if (computerMoveIndex < userMoveIndex){
+        console.log(computerMoveIndex, userMoveIndex);
         alert("HUMAN WINS");
+    }
+    else{
+        alert("WRONG INPUT HUMAN")
     }
 
 }
 
-for(let i = 0; i<5; i++){
+for(let i = 0; i<1; i++){
     letsPlay();
 }
