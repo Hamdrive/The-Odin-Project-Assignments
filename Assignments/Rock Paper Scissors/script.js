b6aec125b53635c00ec0cc6a972fc8d20c1c8c50
Hamza <hamza.husein017@gmail.com>
2021-03-11 18:24:08 +0530

    User move input

--- Assignments/Rock Paper Scissors/script.js
+++ Assignments/Rock Paper Scissors/script.js
@@ -1,7 +1,29 @@
 playableMoves = ['ROCK', 'PAPER','SCISSORS']
 
-computerPlays = () => {
+let computerPlay = () => {
     moveGenerator = Math.floor(Math.random() * 3);
-    movePlayed = playableMoves[moveGenerator];
-    return movePlayed;
+    computerMovePlayed = playableMoves[moveGenerator];
+    return computerMovePlayed;
+}
+
+let letsPlay = () => {
+    let userPlay = prompt("Enter your move, Human").toUpperCase();
+    let computer = computerPlay();
+    
+    let userMoveIndex = playableMoves.indexOf(userPlay);
+    let computerMoveIndex = playableMoves.indexOf(computer)
+    if(computerMoveIndex === userMoveIndex){
+        alert("IT'S A TIE")
+    }
+    else if (computerMoveIndex > userMoveIndex){
+        alert("COMPUTER WINS");
+    }
+    else{
+        alert("HUMAN WINS");
+    }
+
+}
+
+for(let i = 0; i<5; i++){
+    letsPlay();
 }
