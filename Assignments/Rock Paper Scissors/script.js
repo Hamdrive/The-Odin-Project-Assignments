fa5c7d76aee0926dd8b9f5a77a6ea14295d49305
Hamza <hamza.husein017@gmail.com>
2021-03-14 22:18:56 +0530

    allow user input using buttons

--- Assignments/Rock Paper Scissors/script.js
+++ Assignments/Rock Paper Scissors/script.js
@@ -1,5 +1,9 @@
 let playableMoves = ["ROCK", "PAPER", "SCISSORS"];
+let userScore = 0;
+let computerScore = 0;
+let round = 1;
 
+let img = document.querySelectorAll("img")
 let rockmove = document.querySelector(".rockimg");
 let papermove = document.querySelector(".paperimg");
 let scissorsmove = document.querySelector(".scissorsimg");
@@ -9,3 +13,19 @@ let computerMove = () => {
   computerMovePlayed = playableMoves[moveGenerator];
   return computerMovePlayed;
 };
+
+let letsPlay = () => {
+    let computerPlay = computerMove();
+    let userPlay = userMove.toUpperCase();
+    console.log(computerPlay, userPlay);
+}
+
+let click = () => {
+    img.forEach((img) =>
+      img.addEventListener("click", (e) => letsPlay(e.target.id))
+    );
+}
+
+rockmove.addEventListener("click", (e) => letsPlay(e.target.id), false);
+papermove.addEventListener("click", (e) => letsPlay(e.target.id));
+scissorsmove.addEventListener("click", (e) => letsPlay(e.target.id));
