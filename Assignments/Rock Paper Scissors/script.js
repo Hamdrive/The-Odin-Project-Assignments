9f535c0a8c448e20e240036012ec3c6ab904b366
Hamza <hamza.husein017@gmail.com>
2021-03-14 18:11:32 +0530

    import images, set static texts

--- Assignments/Rock Paper Scissors/script.js
+++ Assignments/Rock Paper Scissors/script.js
@@ -1,2 +1,11 @@
 let playableMoves = ["ROCK", "PAPER", "SCISSORS"];
 
+let rockmove = document.querySelector(".rockimg");
+let papermove = document.querySelector(".paperimg");
+let scissorsmove = document.querySelector(".scissorsimg");
+
+let computerMove = () => {
+  moveGenerator = Math.floor(Math.random() * 3);
+  computerMovePlayed = playableMoves[moveGenerator];
+  return computerMovePlayed;
+};
