d063eda8aa8c5ea95c5a1b16e2fb999681876416
Hamza <hamza.husein017@gmail.com>
2021-03-14 22:29:13 +0530

    conditionals

--- Assignments/Rock Paper Scissors/script.js
+++ Assignments/Rock Paper Scissors/script.js
@@ -3,7 +3,6 @@ let userScore = 0;
 let computerScore = 0;
 let round = 1;
 
-let img = document.querySelectorAll("img")
 let rockmove = document.querySelector(".rockimg");
 let papermove = document.querySelector(".paperimg");
 let scissorsmove = document.querySelector(".scissorsimg");
@@ -14,18 +13,67 @@ let computerMove = () => {
   return computerMovePlayed;
 };
 
-let letsPlay = () => {
-    let computerPlay = computerMove();
-    let userPlay = userMove.toUpperCase();
-    console.log(computerPlay, userPlay);
+let letsPlay = userMove => {
+    if(round < 6){
+        let computerPlay = computerMove();
+        let userPlay = userMove.toUpperCase();
+
+        console.log(computerPlay, userPlay);
+        if (computerPlay === userPlay) {
+          console.log(computerScore, userScore);
+          
+        } else if (computerPlay === "ROCK") {
+          if (userPlay === "PAPER") {
+            userScore++;
+            console.log(computerScore, userScore);
+            
+          }
+          if (userPlay === "SCISSORS") {
+            computerScore++;
+            console.log(computerScore, userScore);
+            
+          }
+        } else if (computerPlay === "PAPER") {
+          if (userPlay === "SCISSORS") {
+            userScore++;
+            console.log(computerScore, userScore);
+            
+          }
+          if (userPlay === "ROCK") {
+            computerScore++;
+            console.log(computerScore, userScore);
+            
+          }
+        } else if (computerPlay === "SCISSORS") {
+          if (userPlay === "ROCK") {
+            userScore++;
+            console.log(computerScore, userScore);
+            
+          }
+          if (userPlay === "PAPER") {
+            computerScore++;
+            console.log(computerScore, userScore);
+            
+          }
+        }
+
+        round++;
+    }
+
+    if (round > 5) {
+      if (computerScore > userScore) {
+        alert("COMPUTER IS VICTORIOUS!");
+      } else if (computerScore < userScore) {
+        alert("HUMAN IS VICTORIOUS!");
+      } else {
+        alert("NO ONE IS VICTORIOUS!");
+      }
+    }
+    
 }
 
-let click = () => {
-    img.forEach((img) =>
-      img.addEventListener("click", (e) => letsPlay(e.target.id))
-    );
-}
-
-rockmove.addEventListener("click", (e) => letsPlay(e.target.id), false);
+rockmove.addEventListener("click", (e) => letsPlay(e.target.id));
 papermove.addEventListener("click", (e) => letsPlay(e.target.id));
 scissorsmove.addEventListener("click", (e) => letsPlay(e.target.id));
+
+
