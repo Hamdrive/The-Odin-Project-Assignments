4641ac2d6f5cd50156d7dd7692ac4b7bae67e2e4
Hamza <hamza.husein017@gmail.com>
2021-03-29 23:11:09 +0530

    implement local storage

--- Assignments/Book Memo/script.js
+++ Assignments/Book Memo/script.js
@@ -9,8 +9,36 @@ const newBook = (bookName, authorName, date) => {
     return { bookName, authorName, date };
 };
 
-//Add book to table
+//Local Storage
 
+const getBooksFromLocal = () => {
+    let books;
+    if (localStorage.getItem("books") ===  null){
+        books = [];
+    }
+    else{
+        books = (localStorage.getItem("books"));
+        console.log(books);
+    }
+    return books;
+}
+
+const addBookToLocal = (book) => {
+    const books = getBooksFromLocal();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+const removeBookFromLocal = (bookName) => {
+    const books = getBooksFromLocal();
+    books.forEach((book, index) => {if(book.bookName === bookName){
+        books.splice(index, 1);
+    }})
+
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+//Add book to table
 const addBook = (book) => {
     
     const bookList = document.getElementById("book-readlist");
@@ -24,23 +52,29 @@ const addBook = (book) => {
     document.getElementById("bookform").reset();
 };
 
+
 //Event call to add book to table on submit
 document.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById("bookname").value;
-    const author = document.getElementById("bookauthor").value;
-    const date = document.getElementById("bookdate").value;
-    
-    const book = newBook(name, author, date);
-    
-    addBook(book);
+  e.preventDefault();
+  const name = document.getElementById("bookname").value;
+  const author = document.getElementById("bookauthor").value;
+  const date = document.getElementById("bookdate").value;
+
+  const book = newBook(name, author, date);
+
+  addBook(book);
+  //Add book to local storage
+  addBookToLocal(book);
 })
 
 //Remove book from table
 const deleteBook = del => {
-    if (del.classList.contains('delete')){
-        del.parentElement.parentElement.remove();
-    }
+  if (del.classList.contains("delete")) {
+    del.parentElement.parentElement.remove();
+  }
+  console.log(del.parentElement.parentElement.getElementById("bookname"));
+  //Remove book to local storage
+//   removeBookFromLocal();
 }
 
 //Event call to remove a book on clicking X
