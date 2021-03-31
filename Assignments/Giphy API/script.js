034faf43bc1ed46eafa138b6d61e2b034896ff9f
Hamza <hamza.husein017@gmail.com>
2021-03-31 16:47:00 +0530

    utilize async/await

--- Assignments/Giphy API/script.js
+++ Assignments/Giphy API/script.js
@@ -1,9 +1,20 @@
 let img = document.querySelector("img");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=o6TrRlu0XXORwd6tLPE2wEPqNAHFCBhT&s=cats",
-  { mode: "cors" }
-)
-  .then((response) => response.json())
-  .then((response) => {img.src = response.data.images.original.url;});
+// fetch(
+//   "https://api.giphy.com/v1/gifs/translate?api_key=o6TrRlu0XXORwd6tLPE2wEPqNAHFCBhT&s=cats",
+//   { mode: "cors" }
+// )
+//   .then((response) => response.json())
+//   .then((response) => {img.src = response.data.images.original.url;});
+
+const getCats = async () => {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=o6TrRlu0XXORwd6tLPE2wEPqNAHFCBhT&s=random",
+      { mode: "cors" }
+    );
+    const catGIF = await(response.json());
+    img.src = catGIF.data.images.original.url;
+}
+
+getCats()
     
