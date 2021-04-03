4d4e0435510cd2a91b66dfb177b6da9f2b7d60fe
Hamza <hamza.husein017@gmail.com>
2021-04-03 15:19:47 +0530

    add delete button

--- Assignments/basic submit app/src/components/Overview.js
+++ Assignments/basic submit app/src/components/Overview.js
@@ -1,17 +1,28 @@
 import React from "react";
 import uniqid from "uniqid";
+import { IconContext } from "react-icons";
+import { AiFillDelete } from "react-icons/ai";
 
 const Overview = (props) =>{
     const {tasks} = props;
 
     return(
-        <ul>
+        <ol>
             
-        {tasks.map((task) => {
-            return <li key={uniqid()}>{task}</li>
+        {tasks.map((task, index) => {
+            return (
+              <li key={uniqid()} style={{display: "flex", alignItems: "center", fontSize: "22px"}}>
+                {index+1 + ".   "}{task}
+                <IconContext.Provider value={{ color: "red", size: "1em"}}>
+                  <div>
+                    <AiFillDelete />
+                  </div>
+                </IconContext.Provider>
+              </li>
+            );
         })}
             
-        </ul>
+        </ol>
     )
 }
 
