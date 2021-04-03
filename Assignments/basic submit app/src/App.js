bd8b8dd9e9da8e0cad90b9ae74ddf7b13156d7fb
Hamza <hamza.husein017@gmail.com>
2021-04-03 14:18:14 +0530

    render form element

--- Assignments/basic submit app/src/App.js
+++ Assignments/basic submit app/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state={
+      task: "",
+      tasks: [],
+    };
+  }
   
   render() {
     return (
       <div>
-        Hello World
+        <form>
+          <label htmlFor="textInput">Enter Item</label>
+          <input type="text" id="textInput"/>
+          <button type="submit">Add to List</button>
+        </form>
       </div>
     );
   }
