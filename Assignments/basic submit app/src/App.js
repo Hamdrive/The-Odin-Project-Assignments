817059636e0cb8c17668ef58cdd49aae2be7a019
Hamza <hamza.husein017@gmail.com>
2021-04-03 13:34:44 +0530

    setup components

--- Assignments/basic submit app/src/App.js
+++ Assignments/basic submit app/src/App.js
@@ -1,8 +1,14 @@
+import React, { Component } from "react";
 
-import './App.css';
-
-function App() {
-  return (<div></div>);
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
 }
 
 export default App;
