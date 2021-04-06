b8bb685eb4cf601002853f4a86b9630f67923a5f
Hamza <hamza.husein017@gmail.com>
2021-04-06 23:14:47 +0530

    implement states

--- Assignments/cv/src/App.js
+++ Assignments/cv/src/App.js
@@ -27,6 +27,11 @@ class App extends Component {
       workExp: "",
       userCV: [],
     };
+
+    this.onHandleChange = this.onHandleChange.bind(this);
+    this.reqdInfoSubmitChange = this.reqdInfoSubmitChange.bind(this);
+    this.edExpSubmitChange = this.edExpSubmitChange.bind(this);
+    this.workExpSubmitChange = this.workExpSubmitChange.bind(this);
   }
 
   onHandleChange = (e) => {
@@ -41,60 +46,78 @@ class App extends Component {
   reqdInfoSubmitChange = (e) => {
     e.preventDefault();
 
-    this.setState({
-      edExp: {
-        fName: this.state.fName,
-        lName: this.state.lName,
-        userEmail: this.state.userEmail,
-        mobNumber: this.state.mobNumber,
-        DOB: this.state.DOB,
+    this.setState(
+      {
+        reqdInfo: {
+          fName: this.state.fName,
+          lName: this.state.lName,
+          userEmail: this.state.userEmail,
+          mobNumber: this.state.mobNumber,
+          DOB: this.state.DOB,
+        },
+        fName: "",
+        lName: "",
+        userEmail: "",
+        mobNumber: "",
+        DOB: "",
+        
+        // userCV: this.state.userCV.concat(this.state.reqdInfo),
       },
-      userCV: this.state.userCV.concat(this.state.reqdInfo),
-      fName: "",
-      lName: "",
-      userEmail: "",
-      mobNumber: "",
-      DOB: "",
-    });
+      () => console.log(this.state.reqdInfo),
+    );
   };
 
   edExpSubmitChange = (e) => {
     e.preventDefault();
 
-    this.setState({
-      edExp: {
-        studyLoc: this.state.studyLoc,
-        studyField: this.state.studyField,
-        startStudy: this.state.startStudy,
-        endStudy: this.state.endStudy,
+    this.setState(
+      {
+        edExp: {
+          studyLoc: this.state.studyLoc,
+          studyField: this.state.studyField,
+          startStudy: this.state.startStudy,
+          endStudy: this.state.endStudy,
+        },
+        // userCV: this.state.userCV.concat(this.state.edExp),
+        studyLoc: "",
+        studyField: "",
+        startStudy: "",
+        endStudy: "",
       },
-      userCV: this.state.userCV.concat(this.state.edExp),
-      studyLoc: "",
-      studyField: "",
-      startStudy: "",
-      endStudy: "",
-    });
+      () => console.log(this.state.edExp),
+    );
   };
 
   workExpSubmitChange = (e) => {
     e.preventDefault();
 
-    this.setState({
-      workExp: {
-        compName: this.state.compName,
-        compTitle: this.state.compTitle,
-        startWork: this.state.startWork,
-        endWork: this.state.endWork,
-        workSummary: this.state.workSummary,
+    this.setState(
+      {
+        workExp: {
+          compName: this.state.compName,
+          compTitle: this.state.compTitle,
+          startWork: this.state.startWork,
+          endWork: this.state.endWork,
+          workSummary: this.state.workSummary,
+        },
+        // userCV: this.state.userCV.concat(this.state.workExp),
+        compName: "",
+        compTitle: "",
+        startWork: "",
+        endWork: "",
+        workSummary: "",
       },
-      userCV: this.state.userCV.concat(this.state.workExp),
-      compName: "",
-      compTitle: "",
-      startWork: "",
-      endWork: "",
-      workSummary: "",
-    });
+      () => console.log(this.state.workExp),
+    );
   };
+
+  cvAddition = (e) =>{
+    e.preventDefault();
+
+    this.setState({
+      userCV : {...this.state.reqdInfo, ...this.state.edExp, ...this.state.workExp}
+    }, () => console.log(this.state.userCV),)
+  }
 
   render() {
     return (
@@ -114,13 +137,23 @@ class App extends Component {
                 type="text"
                 name="fName"
                 id="fNameInput"
+                value={this.state.fName}
+                onChange={this.onHandleChange}
                 placeholder="First name"
                 required
               />
             </div>
             <div className="lName">
               <label htmlFor="lName">Last Name:</label>
-              <input type="text" name="lName" id="lNameInput" placeholder="Last name" required />
+              <input
+                type="text"
+                name="lName"
+                id="lNameInput"
+                value={this.state.lName}
+                onChange={this.onHandleChange}
+                placeholder="Last name"
+                required
+              />
             </div>
             <div className="userEmail">
               <label htmlFor="useremail">Email:</label>
@@ -128,6 +161,8 @@ class App extends Component {
                 type="email"
                 name="userEmail"
                 id="userEmailInput"
+                value={this.state.userEmail}
+                onChange={this.onHandleChange}
                 placeholder="example@abc.com"
                 required
               />
@@ -138,15 +173,26 @@ class App extends Component {
                 type="number"
                 name="mobNumber"
                 id="mobumberInput"
+                value={this.state.mobNumber}
+                onChange={this.onHandleChange}
                 placeholder="123-456-789"
                 required
               />
             </div>
             <div className="DOB">
               <label htmlFor="DOB">Date of Birth:</label>
-              <input type="date" name="DOB" id="DOBInput" required />
+              <input
+                type="date"
+                name="DOB"
+                id="DOBInput"
+                value={this.state.DOB}
+                onChange={this.onHandleChange}
+                required
+              />
             </div>
-            <button>Save</button>
+            <button type="submit" onClick={this.reqdInfoSubmitChange}>
+              Save
+            </button>
           </div>
 
           <div className="edExpInfo">
@@ -159,6 +205,8 @@ class App extends Component {
                 type="text"
                 name="studyLoc"
                 id="studyLocInput"
+                value={this.state.studyLoc}
+                onChange={this.onHandleChange}
                 placeholder="University/College Name"
               />
             </div>
@@ -168,18 +216,36 @@ class App extends Component {
                 type="text"
                 name="studyField"
                 id="studyFieldInput"
+                value={this.state.studyField}
+                onChange={this.onHandleChange}
                 placeholder="Field of Study"
               />
             </div>
             <div className="startStudy">
               <label htmlFor="startStudy">From:</label>
-              <input type="date" name="startStudy" id="startStudyInput" />
+              <input
+                type="date"
+                name="startStudy"
+                id="startStudyInput"
+                value={this.state.startStudy}
+                onChange={this.onHandleChange}
+                required
+              />
             </div>
             <div className="endStudy">
               <label htmlFor="endStudy">To:</label>
-              <input type="date" name="endStudy" id="endStudyInput" />
+              <input
+                type="date"
+                name="endStudy"
+                id="endStudyInput"
+                value={this.state.endStudy}
+                onChange={this.onHandleChange}
+                required
+              />
             </div>
-            <button>Save</button>
+            <button type="submit" onClick={this.edExpSubmitChange}>
+              Save
+            </button>
           </div>
 
           <div className="workExpInfo">
@@ -188,7 +254,14 @@ class App extends Component {
             </label>
             <div className="compName">
               <label htmlFor="compName">Company Name:</label>
-              <input type="text" name="compName" id="compNameInput" placeholder="Company Name" />
+              <input
+                type="text"
+                name="compName"
+                id="compNameInput"
+                value={this.state.compName}
+                onChange={this.onHandleChange}
+                placeholder="Company Name"
+              />
             </div>
             <div className="compTitle">
               <label htmlFor="compTitle">Title:</label>
@@ -196,25 +269,52 @@ class App extends Component {
                 type="text"
                 name="compTitle"
                 id="compTitleInput"
+                value={this.state.compTitle}
+                onChange={this.onHandleChange}
                 placeholder="Title at Workplace"
               />
             </div>
             <div className="startWork">
               <label htmlFor="startWork">From:</label>
-              <input type="date" name="startWork" id="startWorkInput" />
+              <input
+                type="date"
+                name="startWork"
+                id="startWorkInput"
+                value={this.state.startWork}
+                onChange={this.onHandleChange}
+                required
+              />
             </div>
             <div className="endWork">
               <label htmlFor="endWork">To:</label>
-              <input type="date" name="endWork" id="endWorkInput" />
+              <input
+                type="date"
+                name="endWork"
+                id="endWorkInput"
+                value={this.state.endWork}
+                onChange={this.onHandleChange}
+                required
+              />
             </div>
             <div className="workSummary">
               <label htmlFor="workSummary">Work Summary:</label>
-              <textarea name="workSummary" id="workSummaryInput" cols="30" rows="10"></textarea>
+              <textarea
+                name="workSummary"
+                id="workSummaryInput"
+                cols="30"
+                rows="10"
+                value={this.state.workSummary}
+                onChange={this.onHandleChange}
+              ></textarea>
             </div>
-            <button>Save</button>
+            <button type="submit" onClick={this.workExpSubmitChange}>
+              Save
+            </button>
           </div>
 
-          <button type="submit">Submit/Print</button>
+          <button type="submit" onClick={this.cvAddition}>
+            Submit/Print
+          </button>
         </form>
 
         {/* <Requiredinfo />
