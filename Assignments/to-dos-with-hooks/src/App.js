2c2cf12d5e66a32fb8b4750a479a67fcf0634134
Hamza <hamza.husein017@gmail.com>
2021-04-08 19:07:57 +0530

    create new todo item

--- Assignments/to-dos-with-hooks/src/App.js
+++ Assignments/to-dos-with-hooks/src/App.js
@@ -2,6 +2,22 @@ import { useState } from "react";
 import ToDo from "./components/ToDo"
 import "./App.css";
 
+const ToDoForm = ({ addToDo }) => {
+  const [todoitem, setTodoitem] = useState("");
+
+  const createToDo = (e) => {
+    e.preventDefault();
+    addToDo(todoitem);
+    setTodoitem("")
+  };
+
+  return (
+    <form onSubmit={createToDo}>
+      <input type="text" name="text" id="text" value={todoitem} onChange={(e) => setTodoitem(e.target.value)}/>
+    </form>
+  );
+};
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -9,14 +25,21 @@ function App() {
     { text: "Complete writeups" },
     {text: "Listen to 2000s songs"}
   ]);
+
+  const addToDo = (todoitem) => {
+    const newToDoList = [...todos, {text: todoitem}];
+    setTodos(newToDoList);
+  };
     
   return (
     <div className="App">
       <div className="todomain">
         <label> TO DO LIST</label>
         {todos.map((todo, index) => (
-          <ToDo key={index} todo={todo} />
+          <ToDo key={index} index={index} todo={todo} />
         ))}
+      
+        <ToDoForm addToDo={addToDo}/>
       </div>
     </div>
   ); 
